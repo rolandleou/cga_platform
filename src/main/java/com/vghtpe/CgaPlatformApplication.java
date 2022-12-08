8b4d820dc2d5b62a607b2e329cb6381aae6129f6
DESKTOP-ICC54BK\user <rolandleou@gmail.com>
2022-12-08 21:55:54 +0800

    Initial GCA Platform project

---
+++ src/main/java/com/vghtpe/CgaPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.vghtpe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgaPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CgaPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vghtpe/CgaPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vghtpe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CgaPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
